a1366795dd18da2e85217d13f20355e7c5bf3f76
BuildTools <unconfigured@null.spigotmc.org>
2022-10-30 17:23:43 +0800

    rua

--- src/main/java/me/xjqsh/lesraisinscore/modes/LesMode.java
+++ src/main/java/me/xjqsh/lesraisinscore/modes/LesMode.java
@@ -2,9 +2,11 @@ package me.xjqsh.lesraisinscore.modes;
 
 /**游戏模式，作为游戏流程控制器*/
 public abstract class LesMode {
+
     public boolean start(){
         return true;
     }
+
     public boolean end(){
         return true;
     }
--- src/main/java/me/xjqsh/utils/ScoreboardTeam.java
+++ src/main/java/me/xjqsh/utils/ScoreboardTeam.java
@@ -1,11 +1,11 @@
 package me.xjqsh.utils;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
 
 public class ScoreboardTeam {
+    /**便捷初始化队伍并进行默认设置*/
     public static void register(Scoreboard scoreboard, ChatColor color){
         Team team = scoreboard.registerNewTeam(color.name());
         team.setColor(color);
